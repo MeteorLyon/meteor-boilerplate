4f881539068fc3130ec5e388a4463f730c911f9e
Benjamin RICHARD <richard.tribes@gmail.com>
2015-11-27 11:54:56 +0100

    fix build scripts, add default buildfolder in package.json

--- tools/bin/build.bat
+++ tools/bin/build.bat
@@ -18,12 +18,6 @@ IF [%NPM_PACKAGE_CONFIG_ROOTURL%] NEQ [] (
     echo "No rooturl config found in package.json"
 )
 
-IF [%NPM_PACKAGE_CONFIG_PORT%] NEQ [] (
-    echo "set port" && (SET PORT=%NPM_PACKAGE_CONFIG_PORT%)
-) ELSE (
-    echo "No port config found in package.json"
-)
-
 IF [%NPM_PACKAGE_CONFIG_PACKAGEDIRS%] NEQ [] (
     echo "set packagedirs" && (SET PACKAGE_DIRS=%NPM_PACKAGE_CONFIG_PACKAGEDIRS%)
 ) ELSE (
@@ -36,10 +30,10 @@ IF [%NPM_PACKAGE_CONFIG_MOBILESERVER%] NEQ [] (
     echo "No mobile server config found in package.json (mobile app offline)" && (SET REBOLON_MOBILE_SERVER="")
 )
 
-IF [%NPM_PACKAGE_CONFIG_BUILDFOLDER%] == [] echo "a build folder before running the build system!" && exit /b 1
+IF [%NPM_PACKAGE_CONFIG_BUILDFOLDER%] NEQ [] echo "a build folder before running the build system!" && exit /b 1
 
 IF [%NPM_PACKAGE_CONFIG_SETTINGSFILE%] NEQ [] (
-    meteor %NPM_PACKAGE_CONFIG_BUILDFOLDER% %REBOLON_MOBILE_SERVER% --mobile-settings %NPM_PACKAGE_CONFIG_SETTINGSFILE% --port %PORT% %*
+    meteor build %NPM_PACKAGE_CONFIG_BUILDFOLDER% %REBOLON_MOBILE_SERVER% --mobile-settings %NPM_PACKAGE_CONFIG_SETTINGSFILE% %*
 ) ELSE (
-    meteor %NPM_PACKAGE_CONFIG_BUILDFOLDER% %REBOLON_MOBILE_SERVER% --port %PORT% %*
+    meteor build %NPM_PACKAGE_CONFIG_BUILDFOLDER% %REBOLON_MOBILE_SERVER% %*
 )
--- tools/bin/build.sh
+++ tools/bin/build.sh
@@ -3,7 +3,6 @@
 if [ -z "$npm_package_config_mongourl" ]; then echo "No mongourl config found in package.json"; else echo "set mongourl" && export MONGO_URL=$npm_package_config_mongourl; fi
 if [ -z "$npm_package_config_mailurl" ]; then echo "No mailurl config found in package.json"; else echo "set mailurl" && export MAIL_URL=$npm_package_config_mailurl; fi
 if [ -z "$npm_package_config_rooturl" ]; then echo "No rooturl config found in package.json"; else echo "set rooturl" && export ROOT_URL=$npm_package_config_rooturl; fi
-if [ -z "$npm_package_config_port" ]; then echo "No port config found in package.json"; else echo "set port" && export PORT=$npm_package_config_port; fi
 if [ -z "$npm_package_config_packagedirs" ]; then echo "No packagedirs config found in package.json"; else echo "set packagedirs" && export PACKAGE_DIRS=$npm_package_config_packagedirs; fi
 if [ -z "$npm_package_config_mobileserver" ]; then echo "No mobile server config found in package.json (mobile app offline)" && export REBOLON_MOBILE_SERVER=""; else echo "set mobile server" && export REBOLON_MOBILE_SERVER=" --server $npm_package_config_packagedirs"; fi
 
@@ -12,4 +11,4 @@ if [ -z "$npm_package_config_buildfolder" ]; then (
     exit 1
 ) fi
 
-if [ -z "$npm_package_config_settingsfile" ]; then meteor $npm_package_config_buildfolder $REBOLON_MOBILE_SERVER --port $PORT $*; else meteor $npm_package_config_buildfolder $REBOLON_MOBILE_SERVER --mobile-settings $npm_package_config_settingsfile --port $PORT $*; fi
+if [ -z "$npm_package_config_settingsfile" ]; then meteor build $npm_package_config_buildfolder $REBOLON_MOBILE_SERVER $*; else meteor build $npm_package_config_buildfolder $REBOLON_MOBILE_SERVER --mobile-settings $npm_package_config_settingsfile $*; fi
