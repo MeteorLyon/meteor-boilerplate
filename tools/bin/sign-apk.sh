8d72ac993ec43d232b18309f192526b41eba50eb
Benjamin RICHARD <richard.tribes@gmail.com>
2015-11-27 12:18:05 +0100

    add sign.sh to sign your android apk

---
+++ tools/bin/sign-apk.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+if [ -z "$npm_package_config_javakeystore" ]; then (
+    echo "No java keystore found config found in package.json (mobile app)"
+    exit 1
+) fi
+
+if [ -z "$npm_package_config_buildfolder" ]; then (
+    echo "No build folder config found in package.json (mobile app)"
+    exit 1
+) fi
+
+if [ -z "$npm_package_config_javakeystorealiasname" ]; then (
+    echo "set an alias name used when you generate your keystore"
+    exit 1
+) fi
+
+jarsigner -sigalg SHA1withRSA -digestalg SHA1 -keystore $npm_package_config_javakeystore $npm_package_config_buildfolder/release-unaligned.apk $npm_package_config_javakeystorealiasname
