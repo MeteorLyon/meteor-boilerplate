a28b55efc94d09bb86054a061820cc32b343ec27
Benjamin RICHARD <richard.tribes@gmail.com>
2015-12-27 22:48:02 +0100

    Add a script for debug

---
+++ tools/bin/debug.bat
@@ -0,0 +1,39 @@
+@echo off
+
+IF [%NPM_PACKAGE_CONFIG_MONGOURL%] NEQ [] (
+    echo "set mongourl" && (SET MONGO_URL=%NPM_PACKAGE_CONFIG_MONGOURL%)
+) ELSE (
+    echo "No mongourl config found in package.json"
+)
+
+IF [%NPM_PACKAGE_CONFIG_MAILURL%] NEQ [] (
+    echo "set mailurl" && (SET MAIL_URL=%NPM_PACKAGE_CONFIG_MAILURL%)
+) ELSE (
+    echo "No mailurl config found in package.json"
+)
+
+IF [%NPM_PACKAGE_CONFIG_ROOTURL%] NEQ [] (
+    echo "set rooturl" && (SET ROOT_URL=%NPM_PACKAGE_CONFIG_ROOTURL%)
+) ELSE (
+    echo "No rooturl config found in package.json"
+)
+
+IF [%NPM_PACKAGE_CONFIG_PORT%] NEQ [] (
+    echo "set port" && (SET PORT=%NPM_PACKAGE_CONFIG_PORT%)
+) ELSE (
+    echo "No port config found in package.json"
+)
+
+IF [%NPM_PACKAGE_CONFIG_PACKAGEDIRS%] NEQ [] (
+    echo "set packagedirs" && (SET PACKAGE_DIRS=%NPM_PACKAGE_CONFIG_PACKAGEDIRS%)
+) ELSE (
+    echo "No packagedirs config found in package.json"
+)
+
+IF [%NPM_PACKAGE_CONFIG_SETTINGSFILE%] NEQ [] (
+    echo "set settings" && (SET REBOLON_SETTINGS=--settings %NPM_PACKAGE_CONFIG_SETTINGSFILE%)
+) ELSE (
+    echo "No settings found in package.json" && (SET REBOLON_SETTINGS="")
+)
+
+meteor debug %REBOLON_SETTINGS% --port %PORT% %*
